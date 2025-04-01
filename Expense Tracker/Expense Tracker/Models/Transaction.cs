758c48df05cc39a666153888f09dd42fa2514278
PedroMesquitaa <pedrozd45@gmail.com>
2025-04-01 19:22:00 -0300

    Fazendo sincronizaçao do Banco de Dados

--- Expense Tracker/Expense Tracker/Models/Transaction.cs
+++ Expense Tracker/Expense Tracker/Models/Transaction.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Expense_Tracker.Models
 {
@@ -7,5 +8,15 @@ namespace Expense_Tracker.Models
         [Key]
         public int TransactionId { get; set; }
 
+        //CategoryId
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+
+        public int Amount { get; set; }
+
+        [Column(TypeName = "nvarchar(5)")]
+        public string? Note { get; set; }
+
+        public DateTime Date { get; set; } = DateTime.Now;
     }
 }
